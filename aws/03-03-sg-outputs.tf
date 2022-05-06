0e42fdaae649d61a4602cf39b21424f66c36925f
AhilanAshwin <ahilanaashwin@gmail.com>
2022-05-06 20:01:39 +0800

    Added security group configuration and init configuration of alb load balancer

--- aws/03-03-sg-outputs.tf
+++ aws/03-03-sg-outputs.tf
@@ -2,38 +2,38 @@
 
 # Public Bastion Host Security Group Outputs
 ## public_bastion_sg_group_id
-output "ecs_sg_group_id" {
+output "alb_sg_group_id" {
   description = "The ID of the security group"
-  value       = module.alb-security-group.this_security_group_id
+  value       = module.alb-security-group.security_group_id
 }
 
 ## public_bastion_sg_group_vpc_id
-output "ecs_sg_group_vpc_id" {
+output "alb_sg_group_vpc_id" {
   description = "The VPC ID"
-  value       = module.alb-security-group.this_security_group_vpc_id
+  value       = module.alb-security-group.security_group_vpc_id
 }
 
 ## public_bastion_sg_group_name
-output "ecs_sg_group_name" {
+output "alb_sg_group_name" {
   description = "The name of the security group"
-  value       = module.alb-security-group.this_security_group_name
+  value       = module.alb-security-group.security_group_name
 }
 
-# Private EC2 Instances Security Group Outputs
+# Private ECS Instances Security Group Outputs
 ## private_sg_group_id
 output "ecs_sg_group_id" {
   description = "The ID of the security group"
-  value       = module.ecs-security-group.this_security_group_id
+  value       = module.ecs-security-group.security_group_id
 }
 
 ## private_sg_group_vpc_id
 output "ecs_sg_group_vpc_id" {
   description = "The VPC ID"
-  value       = module.ecs-security-group.this_security_group_vpc_id
+  value       = module.ecs-security-group.security_group_vpc_id
 }
 
 ## private_sg_group_name
 output "ecs_sg_group_name" {
   description = "The name of the security group"
-  value       = module.ecs-security-group.this_security_group_name
+  value       = module.ecs-security-group.security_group_name
 }
