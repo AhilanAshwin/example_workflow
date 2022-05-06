aab4da80cd617a1f36d97f127b8404311c4d7e37
AhilanAshwin <ahilanaashwin@gmail.com>
2022-05-06 15:57:04 +0800

    Renamed back to main.tf

--- aws/modules/groups/main.tf
+++ aws/modules/groups/main.tf
@@ -1,6 +1,7 @@
-module "security-group" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "4.9.0"
-  vpc_id  = module.vpc.vpc_id
-
+module "ecs_security-group" {
+  source      = "terraform-aws-modules/security-group/aws"
+  version     = "4.9.0"
+  description = "Security group with HTTP & SSH port open for entire VPC block (IPv4 CIDR), egress ports are all world open"
+  vpc_id      = module.vpc.vpc_id
+  name        = var.ecs_group_name
 }
---
+++ aws/modules/groups/variables.tf
@@ -0,0 +1,4 @@
+variable "group_name" {
+  description = "Name of the security group"
+  type        = string
+}
