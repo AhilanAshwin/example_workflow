415422e44c34ce3659fe21e78ab686f0fe3e2175
AhilanAshwin <ahilanaashwin@gmail.com>
2022-05-06 18:16:56 +0800

    Restructuring aws

--- aws/modules/groups/main.tf
+++
@@ -1,8 +0,0 @@
-module "ecs_security-group" {
-  source      = "terraform-aws-modules/security-group/aws"
-  version     = "4.9.0"
-  description = "Security group with HTTP & SSH port open for entire VPC block (IPv4 CIDR), egress ports are all world open"
-
-  vpc_id = module.vpc.vpc_id
-  name   = var.ecs_group_name
-}
